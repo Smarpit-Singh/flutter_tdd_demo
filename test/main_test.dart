ffabfbef53850664abf9abedfbfddc204f51737b
Smarpit <smarpitsingh560@gmail.com>
2021-04-26 13:34:16 +0530

    find by key completed

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,21 +1,29 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_tdd_demo/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-
   //For testing a widget
-  testWidgets('MyWidget has a title and Message', (WidgetTester tester) async {
-
+  testWidgets('Find a Text widget', (WidgetTester tester) async {
     // Create the widget by telling the tester to build it.
-    await tester.pumpWidget(MyWidget(title: 'T', message: 'M',));
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: Text('H'),
+      ),
+    ));
 
-    // Create the Finders.
-    final titleFinder = find.text('T');
-    final messageFinder = find.text('M');
+    //Find one widget with H text
+    expect(find.text('H'), findsOneWidget);
+  });
 
-    // Use the `findsOneWidget` matcher provided by flutter_test to verify
-    // that the Text widgets appear exactly once in the widget tree.
-    expect(titleFinder, findsOneWidget);
-    expect(messageFinder, findsOneWidget);
+  testWidgets('Find a widget using a Key', (WidgetTester tester) async {
+    final testKey = Key('K');
+
+    await tester.pumpWidget(MaterialApp(
+      key: testKey,
+      home: Container(),
+    ));
+
+    expect(find.byKey(testKey), findsOneWidget);
   });
 }
