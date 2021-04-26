3471e54f7bd9006a25cc71924df677d82577fa20
Smarpit <smarpitsingh560@gmail.com>
2021-04-26 11:55:15 +0530

    widget finder test completed

--- test/main_test.dart
+++ test/main_test.dart
@@ -8,5 +8,14 @@ void main() {
 
     // Create the widget by telling the tester to build it.
     await tester.pumpWidget(MyWidget(title: 'T', message: 'M',));
+
+    // Create the Finders.
+    final titleFinder = find.text('T');
+    final messageFinder = find.text('M');
+
+    // Use the `findsOneWidget` matcher provided by flutter_test to verify
+    // that the Text widgets appear exactly once in the widget tree.
+    expect(titleFinder, findsOneWidget);
+    expect(messageFinder, findsOneWidget);
   });
 }
