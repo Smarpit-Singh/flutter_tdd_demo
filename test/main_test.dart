a3e4fde4bb1b40bb453db70f38e001da23ca7da9
Smarpit <smarpitsingh560@gmail.com>
2021-04-26 13:42:46 +0530

    find by instance

--- test/main_test.dart
+++ test/main_test.dart
@@ -3,27 +3,39 @@ import 'package:flutter_tdd_demo/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  //For testing a widget
-  testWidgets('Find a Text widget', (WidgetTester tester) async {
-    // Create the widget by telling the tester to build it.
-    await tester.pumpWidget(MaterialApp(
-      home: Scaffold(
-        body: Text('H'),
-      ),
-    ));
+  group('Widget tests', () {
+    //For testing a widget
+    testWidgets('Find a Text widget', (WidgetTester tester) async {
+      // Create the widget by telling the tester to build it.
+      await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+          body: Text('H'),
+        ),
+      ));
 
-    //Find one widget with H text
-    expect(find.text('H'), findsOneWidget);
-  });
+      //Find one widget with H text
+      expect(find.text('H'), findsOneWidget);
+    });
 
-  testWidgets('Find a widget using a Key', (WidgetTester tester) async {
-    final testKey = Key('K');
+    testWidgets('Find a widget using a Key', (WidgetTester tester) async {
+      final testKey = Key('K');
 
-    await tester.pumpWidget(MaterialApp(
-      key: testKey,
-      home: Container(),
-    ));
+      await tester.pumpWidget(MaterialApp(
+        key: testKey,
+        home: Container(),
+      ));
 
-    expect(find.byKey(testKey), findsOneWidget);
+      expect(find.byKey(testKey), findsOneWidget);
+    });
+
+    testWidgets('Find a specific instance', (WidgetTester tester) async {
+      final testWidget = Padding(padding: EdgeInsets.zero);
+
+      await tester.pumpWidget(Container(
+        child: testWidget,
+      ));
+
+      expect(find.byWidget(testWidget), findsOneWidget);
+    });
   });
 }
